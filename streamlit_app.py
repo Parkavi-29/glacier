1589f2ccebfeed8118ee62694c66d74408e942d3
Parkavi-29 <147430784+Parkavi-29@users.noreply.github.com>
2025-05-01 11:42:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.markdown("""
 st.markdown(f"""
 <div style="text-align: center; font-family: 'Catamaran', sans-serif; margin-top: -10px; padding-bottom: 20px;">
     <div style="font-size: 30px; font-weight: bold; color: #0b3954;">Current Date & Time (IST)</div>
-    <div style="font-size: 24px; color: #333;">{current_time_ist}</div>
+    <div style="font-size: 24px; color: #333;"></div>
 </div>
 """, unsafe_allow_html=True)
 
