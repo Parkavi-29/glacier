b2733e11c86c161ec25ff1ba07027aacc4867363
Parkavi-29 <147430784+Parkavi-29@users.noreply.github.com>
2025-05-01 11:59:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ st.markdown("""
 <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@400;600;700&display=swap" rel="stylesheet">
 <style>
 [data-testid="stAppViewContainer"] {
-    background-image: url("https://i.pinimg.com/736x/aa/e4/54/aae454df5a468ca876f6912d496b1b61.jpg");
+    background-image: url("https://i.pinimg.com/736x/bf/05/8f/bf058f329a00bd070962533e91f34d30.jpg");
     background-color: transparent;
     background-size: cover;
     background-attachment: fixed;
